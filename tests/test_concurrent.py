1789c9da26bd9772608e716e80d7a5110968f414
Bertrand Chenal <bertrand.chenal@external.engie.com>
2019-10-17 11:03:02 +0200

    Add concurrency tests

---
+++ tests/test_concurrent.py
@@ -0,0 +1,17 @@
+from conquer import sh
+from concurrent import futures
+
+def test_base():
+    scripts = ['tests/chatty.py'] * 3
+    with futures.ThreadPoolExecutor() as executor:
+        for res in executor.map(sh.python, scripts):
+            assert len(res.stdout.splitlines()) == 2000
+
+def test_pipe():
+    cmd = sh.python + 'tests/chatty.py' | sh.wc
+    args = ['-m', '-w', '-L']
+    expected = [7780, 2000, 3]
+    with futures.ThreadPoolExecutor() as executor:
+        for res, exp in zip(executor.map(cmd, args), expected):
+            assert res.success()
+            assert exp == int(res.stdout)
