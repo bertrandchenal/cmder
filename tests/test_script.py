423678d773ce5fb0ed2352f122f93df5c573f79e
Bertrand Chenal <bertrand@adimian.com>
2019-11-01 09:27:43 +0100

    Add prototype for builtin script

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,4 +1,5 @@
 from conquer import sh, SSH
+from conquer.script import ping
 
 script = 'true && echo "ok"'
 
@@ -10,3 +11,12 @@ def test_remote_script():
     remote = SSH('localhost')
     res = remote(script)
     assert str(res) == 'ok\n'
+
+def test_builtin_script():
+    res = ping()
+    assert str(res) == 'pong\n'
+
+    remote = SSH('localhost')
+    res = ping(remote)
+    assert str(res) == 'pong\n'
+
